85ccfa065a00453fcd2622d233e7c5d6fceb2395
Luckiraj Sisodiya <sisodiyarana07@gmail.com>
2023-05-11 16:59:22 +0530

    Add files via upload

---
+++ Oauth2/src/main/java/com/SpringSecurityConfig.java
@@ -0,0 +1,24 @@
+package com;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		return http
+				.authorizeHttpRequests()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.oauth2Login()
+				.and()
+				.build();
+	}
+
+	
+}
